0862c39351ff76eaff53e60624df571f12772b0b
demo_user <saikukkapalli9@gmail.com>
2026-01-30 19:18:38 +0530

    Add github actions pylint workflow

--- demo.py
+++ demo.py
@@ -1,4 +1,10 @@
- 
-a=10
-b=20
-print(a+b)
+"""Demo file for GitHub Actions pylint test."""
+
+def main():
+    a = 10
+    b = 20
+    print(a + b)
+    print("Hello welcome to the git tutorial!")
+
+if __name__ == "__main__":
+    main()
